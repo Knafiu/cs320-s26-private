8a783c183e6ec9a679cc7bdacc56060cc778add1
Khaleel Nafiu <knafiu2@bu.edu>
2026-01-29 20:03:37 -0500

    Completed assign1 with the 4 functions

--- assigns/assign1/lib/assign1.ml
+++ assigns/assign1/lib/assign1.ml
@@ -1,9 +1,22 @@
 
-let sqrt (_n : int) : int = (* CHANGE _n to n! *)
-  assert false
+let sqrt (n : int) : int = (* CHANGE _n to n! *)
+  let rec help k =
+    if k * k >= n 
+    then k
+    else help(k+1)
+  in 
+  help(0)
 
-let pow (_n : int) (_k : int) : int = (* CHANGE _n to n and _k to k! *)
-  assert false
+
+let pow (n : int) (k : int) : int = (* CHANGE _n to n and _k to k! *)
+  let rec help n k acc = 
+    if k = 0
+    then acc
+
+    else help n (k-1) (acc*n)
+  in
+  help n k 1 
+
 
 let is_ws = function
   | ' ' | '\012' | '\n' | '\r' | '\t' -> true
@@ -32,13 +45,72 @@ let implode_all (css : char list list) : string list =
   in loop [] css
 
 let split_on_ws_helper (_cs : char list) : char list list =
-  assert false
+    let isWhiteSpace c = c = ' ' || c = '\n'
+    in
+    let rec loop chars current result =
+      match chars with
+      | [] -> 
+      if current = [] then
+        List.rev result
+      else
+         List.rev ((List.rev current) :: result)
+      | x :: xs -> 
+        if isWhiteSpace x then
+          if current = [] then
+            loop xs [] result
+          else
+            loop xs [] ((List.rev current) :: result)
+        else
+          loop xs (x :: current) result
+      in 
+        loop _cs [] []
+    
+
 
 let split_on_ws (s : string) : string list =
   implode_all (split_on_ws_helper (explode s))
 
 let eval (_stack : int list) (_prog : string list) : int list =
-  assert false
+  let rec loop stack prog =
+    match prog with
+    | [] -> stack
+    | tok :: rest ->
+        match tok with
+        | "+" ->
+            (match stack with
+             | a :: b :: s' -> loop ((b + a) :: s') rest
+             | _ -> failwith "stack underflow")
+        | "-" ->
+            (match stack with
+             | a :: b :: s' -> loop ((b - a) :: s') rest
+             | _ -> failwith "stack underflow")
+        | "*" ->
+            (match stack with
+             | a :: b :: s' -> loop ((b * a) :: s') rest
+             | _ -> failwith "stack underflow")
+        | "/" ->
+            (match stack with
+             | a :: b :: s' -> loop ((b / a) :: s') rest
+             | _ -> failwith "stack underflow")
+        | "mod" ->
+            (match stack with
+             | a :: b :: s' -> loop ((b mod a) :: s') rest
+             | _ -> failwith "stack underflow")
+        | "sqrt" ->
+            (match stack with
+             | a :: s' -> loop ((sqrt a) :: s') rest
+             | _ -> failwith "stack underflow")
+        | "^" ->
+            (match stack with
+             | a :: b :: s' -> loop ((pow b a) :: s') rest
+             | _ -> failwith "stack underflow")
+        | _ ->
+            (* treat it as an integer token *)
+            let n = int_of_string tok in
+            loop (n :: stack) rest
+  in
+  loop _stack _prog
+ 
 
 let interp (input : string) : int =
   match eval [] (split_on_ws input) with
