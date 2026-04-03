bad88b0646b4fe7cebbc87d6c6ad2b05adfb4b3c
Khaleel Nafiu <knafiu2@bu.edu>
2026-04-03 19:49:27 -0400

    Implement evaluation for literals, let, if, negate, and assert

--- assigns/interp1/lib/interp1.ml
+++ assigns/interp1/lib/interp1.ml
@@ -55,16 +55,96 @@ type dyn_env = value Env.t
 
 (* Type Checking *)
 
-let type_of (ctxt : ctxt) (e : expr) : ty option =
-  ignore (ctxt, e); assert false (* TODO *)
+let rec type_of (ctxt : ctxt) (e : expr) : ty option =
+  match e with
+  | Unit -> Some Unit
+  | Bool _ -> Some Bool
+  | Int _ -> Some Int
+  | Var x -> Env.find_opt x ctxt
+  | Let (x, e1, e2) ->
+      (match type_of ctxt e1 with
+       | Some t1 -> type_of (Env.add x t1 ctxt) e2
+       | None -> None)
+  | LetRec { name; arg; arg_ty; out_ty; binding; body } ->
+      let f_ty = Fun (arg_ty, out_ty) in
+      let ctxt' = ctxt |> Env.add name f_ty |> Env.add arg arg_ty in
+      (match type_of ctxt' binding with
+       | Some t when t = out_ty ->
+           type_of (Env.add name f_ty ctxt) body
+       | _ -> None)
+  | If (e1, e2, e3) ->
+      (match type_of ctxt e1, type_of ctxt e2, type_of ctxt e3 with
+       | Some Bool, Some t2, Some t3 when t2 = t3 -> Some t2
+       | _ -> None)
+  | Fun (x, t1, body) ->
+      (match type_of (Env.add x t1 ctxt) body with
+       | Some t2 -> Some (Fun (t1, t2))
+       | None -> None)
+  | App (e1, e2) ->
+      (match type_of ctxt e1, type_of ctxt e2 with
+       | Some (Fun (arg_ty, out_ty)), Some t2 when arg_ty = t2 -> Some out_ty
+       | _ -> None)
+  | Bop (op, e1, e2) ->
+      (match op, type_of ctxt e1, type_of ctxt e2 with
+       | (Add | Sub | Mul | Div | Mod), Some Int, Some Int -> Some Int
+       | (And | Or), Some Bool, Some Bool -> Some Bool
+       | (Eq | Neq | Lt | Lte | Gt | Gte), Some t1, Some t2 when t1 = t2 -> Some Bool
+       | _ -> None)
+  | Negate e1 ->
+      (match type_of ctxt e1 with
+       | Some Int -> Some Int
+       | _ -> None)
+  | Assert e1 ->
+      (match type_of ctxt e1 with
+       | Some Bool -> Some Unit
+       | _ -> None)
 
 (* Evaluation *)
 
 exception Div_by_zero
 exception Assert_fail
 
-let eval (env : dyn_env) (e : expr) : value =
-  ignore (env, e); assert false (* TODO *)
+let rec eval (env : dyn_env) (e : expr) : value =
+  match e with
+  | Unit -> Unit
+  | Bool b -> Bool b
+  | Int n -> Int n
+  | Var x -> Env.find x env
+  | Let (x, e1, e2) ->
+      let v1 = eval env e1 in
+      eval (Env.add x v1 env) e2
+  | LetRec { name; arg; arg_ty; out_ty = _; binding; body } ->
+      let fun_expr = Fun (arg, arg_ty, binding) in
+      let rec_closure = Clos (env, Some name, fun_expr) in
+      eval (Env.add name rec_closure env) body
+  | If (e1, e2, e3) ->
+      (match eval env e1 with
+       | Bool true -> eval env e2
+       | Bool false -> eval env e3
+       | _ -> assert false)
+  | Fun (_, _, _) ->
+      Clos (env, None, e)
+  | App (e1, e2) ->
+      let vf = eval env e1 in
+      let va = eval env e2 in
+      (match vf with
+       | Clos (clos_env, None, Fun (x, _, body)) ->
+           eval (Env.add x va clos_env) body
+       | Clos (clos_env, Some f, Fun (x, _, body)) ->
+           let env' = clos_env |> Env.add f vf |> Env.add x va in
+           eval env' body
+       | _ -> assert false)
+  | Negate e1 ->
+      (match eval env e1 with
+       | Int n -> Int (-n)
+       | _ -> assert false)
+  | Assert e1 ->
+      (match eval env e1 with
+       | Bool true -> Unit
+       | Bool false -> raise Assert_fail
+       | _ -> assert false)
+  | Bop _ -> assert false
+
 
 (* Interpretation *)
 
